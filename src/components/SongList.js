845960463d3ff2124f4103940952b04f02d93c1e
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-18 15:55:08 +0100

    create list of elements, wire up Action Creator to SongList Component state

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectSong} from "../actions";
 
 class SongList extends React.Component {
-  render() {
-    const songs = this.props.songs.map(song => {
-      return <div>{song.title}</div>;
-    });
 
-    return <div>
-      {songs}
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary" onClick={() => this.props.selectSong(song)}>
+              Select
+            </button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    console.log(this.props)
+    return <div className="ui divided list">
+      {this.renderList()}
     </div>;
   }
 }
@@ -19,4 +35,4 @@ const mapStateToProps = state => {
   };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {selectSong})(SongList);
